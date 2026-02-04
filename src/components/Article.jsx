9c6209b4039665afd425116f2f32efd7a6ce48b4
Candra Icy <candra.icy@gmail.com>
2026-02-04 15:45:34 +0800

    Conditional Rendering 2 menit ke 4

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -2,7 +2,9 @@ function Article(props) {
   return (
     <>
       <h3>{props.title}</h3>
-      <small>Date : {props.date}, tags: {props.tags.join(",")}</small>
+      <small>Date : {props.date}, tags: {props.tags.join(",")} {props.isNew ? "-- Baru" : "lama"}
+
+      </small>
       
     </>
   );
